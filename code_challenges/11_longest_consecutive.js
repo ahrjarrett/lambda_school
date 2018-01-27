a70810235c86aa337fadb4aa78af6fa3883fe945
Andrew Jarrett <ahrjarrett@gmail.com>
2018-01-27 12:47:25 -0700

    add data structures 1 solutions lecture notes, brownbag: imposter syndrome

--- code_challenges/11_longest_consecutive.js
+++ code_challenges/11_longest_consecutive.js
@@ -1,17 +1,11 @@
-function longestConsecutive(arr, k){
-  const lengths = []
-  let longest = 0
+const longestConsecutive = (arr, k) => {
+  if(arr.length === 0 || arr.length < k || k <= 0) return ''
+  const mapped = arr.map((item, index) =>
+    arr.slice(index, index + k).join(''))
 
-  arr.forEach(str => lengths.push(str.length))
-
-  const test = lengths.reduce((acc, curr, i) => {
-    console.log(i)
-
-  })
-
-
-  return test
+  return mapped.sort((a, b) => b.length - a.length)[0]
 }
 
-console.log(longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas"], 1))
-// => "abigailtheta"
+console.log(longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas"], 3))
+// => "zoneabigailtheta"
+console.log(longestConsecutive(["zzonezonezonezonezonezonezonezonezonezonezoneone", "abigail", "theta", "form", "libe", "zas"], 2))
