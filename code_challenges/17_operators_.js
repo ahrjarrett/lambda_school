c4ff976be5ca31b1680acce6bca8905c2746d8fa
Andrew Jarrett <ahrjarrett@gmail.com>
2018-02-08 17:50:26 -0700

    finish code challenge 17: operators

---
+++ code_challenges/17_operators_.js
@@ -0,0 +1,44 @@
+/* Implement three functions called multiply, divide, and modulo.
+ * These functions should multiply, divide, or return the remainder of two arguments.
+ * Now for the tricky part: you can only use + and - to implement these functions. */
+
+const modulo = (x, y) =>
+  x - y < y
+    ? x - y
+    : modulo(x - y, y)
+
+const multiply = (x, y) => {
+  const recurse = (x, y) => (
+    y === 1
+      ? x
+      : x + recurse(x, y - 1)
+  )
+  if (x === 0 || y === 0) return 0
+  if (x < 0 && y < 00) return recurse(-x, -y)
+  if (x > 0 !== y > 0) {
+    if (x > y) return -recurse(x, -y)
+    else return -recurse(-x, y)
+  }
+  return recurse(x, y)
+}
+
+const divide = (x, y) => (function recurse(x, y, result = 0) {
+  let [xx, yy] = [x, y]
+  if(xx < 0 && yy < 0) {
+    xx = -xx
+    yy = -yy
+  }
+
+  if ((xx >= 0) !== (yy >= 0)) {
+    xx = xx > 0 ? xx : -xx
+    yy = yy > 0 ? yy : -yy
+    return xx < yy
+      ? -result
+      : recurse(xx - yy, -yy, result + 1)
+  }
+
+  return xx < yy
+      ? result
+      : recurse(xx - yy, yy, result + 1)
+})(x, y)
+
