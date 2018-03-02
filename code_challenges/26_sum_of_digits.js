1d31b7229e34e1d01030fd134f2b045fb92254b0
Andrew Jarrett <ahrjarrett@gmail.com>
2018-03-02 00:46:23 -0700

    code challenge 26: solve sumOfDigits using tail-call recursion :)

--- code_challenges/26_sum_of_digits.js
+++ code_challenges/26_sum_of_digits.js
@@ -1,16 +1,26 @@
-let callNumber = 0
-const sumOfDigits = n => {
-  callNumber++
-  console.log(n)
-  console.log(Math.floor(n / 10))
-  console.log('+')
-  console.log(n % 10)
-  if(n < 10) return n
-  return Math.floor(n / 10) + sumOfDigits(n % 10)
+/*
+ * Write a function called sumOfDigits that given a positive integer, returns the sum of its digits.Assume all numbers will be positive. 
 
-}
+ * Input Sample: 
+ * 23
+ * 496
 
-//console.log(sumOfDigits(23))  // => 5
-console.log(sumOfDigits(69))  // => 5
+ * Output Sample: 
+ * 5
+ * 19
 
-//console.log(sumOfDigits(496)) // => 19
+ * sumOfDigits(23) => 5 because 2 + 3 = 5. */
+
+
+// implemented using tail-call recursion:
+const { floor } = Math
+
+const sumOfDigits = (n, acc = 0) =>
+  n === 0
+    ? acc
+    : sumOfDigits(floor(n / 10), acc + n % 10)
+
+
+console.log(sumOfDigits(23))   // => 5
+console.log(sumOfDigits(496))  // => 19
+
