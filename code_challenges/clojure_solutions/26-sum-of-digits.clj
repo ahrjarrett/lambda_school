de58396b9485211d5dfc911094bd8a3b333631a0
Andrew Jarrett <ahrjarrett@gmail.com>
2018-03-02 01:15:09 -0700

    solve sum-of-digits in clojure

---
+++ code_challenges/clojure_solutions/26-sum-of-digits.clj
@@ -0,0 +1,19 @@
+;; Write a function called sumOfDigits that given a positive integer, returns the sum of its digits.Assume all numbers will be positive. 
+;; Sample: 
+;; 23  => 5
+;; 496 => 19
+;; sumOfDigits(23) => 5 because 2 + 3 = 5.
+
+;; Note that RECUR handles tail-call recursion for us,
+;; but in this case the function is already written
+;; with the function call in tail position:
+
+(defn sum-of-digits [n]
+  (loop [count n acc 0]
+        (if (zero? count) acc
+            (recur
+              (quot count 10)
+              (+ acc (mod count 10))))))
+
+(sum-of-digits 23)  ;; => 5
+(sum-of-digits 496) ;; => 19
